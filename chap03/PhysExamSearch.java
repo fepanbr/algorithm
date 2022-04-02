eb16782c5f63133275700862b59e247b179f3564
fepanbr <fepanbr@gmail.com>
2022-04-02 18:43:40 +0900

    feat: generic 클래스

--- chap03/PhysExamSearch.java
+++ chap03/PhysExamSearch.java
@@ -22,12 +22,21 @@ public class PhysExamSearch {
     }
 
     public static final Comparator<PhyscData> HEIGHT_ORDER = new HeightOrderComparator();
+    public static final Comparator<PhyscData> VISION_ORDER = new VisionOrderComparator();
 
     private static class HeightOrderComparator implements Comparator<PhyscData> {
       @Override
       public int compare(PhyscData o1, PhyscData o2) {
         return (o1.height > o2.height) ? 1 : ((o1.height < o2.height) ? -1 : 0);
 
+      }
+    }
+
+    private static class VisionOrderComparator implements Comparator<PhyscData> {
+      @Override
+      public int compare(PhyscData o1, PhyscData o2) {
+
+        return o1.vision > o2.vision ? 1 : (o1.vision < o2.vision ? -1 : 0);
       }
     }
   }
@@ -43,16 +52,36 @@ public class PhysExamSearch {
         new PhyscData("이호연", 174, 1.2),
         new PhyscData("이수민", 175, 2.0),
     };
-    System.out.println("몇 cm인 사람을 찾고 있나요? : ");
-    int height = stdIn.nextInt();
-    int idx = Arrays.binarySearch(x, new PhyscData("", height, 0.0), PhyscData.HEIGHT_ORDER);
+    Arrays.sort(x, new Comparator<PhyscData>() {
+      @Override
+      public int compare(PhyscData o1, PhyscData o2) {
+        return o2.height - o1.height;
+      }
+    });
+
+    for (PhyscData s : x) {
+      System.out.println("s: " + s);
+    }
+    // System.out.println("몇 cm인 사람을 찾고 있나요? : ");
+    System.out.println("시력이 몇인 사람을 찾고 있나요? : ");
+    double vision = stdIn.nextDouble();
+    int idx = Arrays.binarySearch(x, new PhyscData("", 0, vision), PhyscData.VISION_ORDER);
 
     if (idx < 0) {
       System.out.println("요소가 없습니다.");
     } else {
-      System.out.println("x[" + idx + "]에 있습니다.");
-      System.out.println("찾은 데이터 : " + x[idx]);
+      System.out.println("찾는 요소는 x[" + idx + "] 입니다.");
     }
+    // int height = stdIn.nextInt();
+    // int idx = Arrays.binarySearch(x, new PhyscData("", height, 0.0),
+    // PhyscData.HEIGHT_ORDER);
+
+    // if (idx < 0) {
+    // System.out.println("요소가 없습니다.");
+    // } else {
+    // System.out.println("x[" + idx + "]에 있습니다.");
+    // System.out.println("찾은 데이터 : " + x[idx]);
+    // }
   }
 
 }
